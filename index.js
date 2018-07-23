4782e790e94ceac4400245c9bf84cda50b75fdab
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-23 21:08:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ function BoardMember(name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
-
   this.veto = function(){
     console.log("No, I must disagree");
   }
-
   this.approve = function() {
     console.log("You can do that!")
   }
